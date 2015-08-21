306d4fea3be6582427f6b6e1660767239b840823
SSS <somuri.sai@gmail.com>
2015-08-21 20:47:53 +0530

    R Programming: Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -6,48 +6,51 @@
 ## new input matrix as its argument.
 
 
-## This function takes as the argument, the matrix whose inverse is to be found
-## and returns a list containing functions to 
+## The makeCacheMatrix function takes as the argument, the matrix whose inverse
+## is to be found and returns a list containing functions to 
 ## 1. Set the matrix.
 ## 2. Get the matrix.
 ## 3. Set the inverse of the matrix.
 ## 4. Get the inverse of the matrix. 
  
-makeCacheMatrix <- function(x = matrix()) {
-  inverse <- NULL
+makeCacheMatrix <- function(x = matrix())
+{    
+    
+    inverse <- NULL
   
-  ## This is first function of the list. By using the "superassignment"
-  ## operator "<<-", any matrix is assigned to the matrix object and a NULL is
-  ## assigned to the inverse matrix object, in the global environment rather
-  ## than the functional environment.
+    ## This is first function of the list. By using the "superassignment"
+    ## operator "<<-", any matrix is assigned to the matrix object and a NULL is
+    ## assigned to the inverse matrix object, in the global environment rather
+    ## than the functional environment.
   
-  set <- function(y){
-    x <<- y
-    inverse <<- NULL
-  }
-  
-  
-  ## This second function of the list returns the matrix object present in the
-  ## global environment if the set function above is called beforehand. Else it
-  ## returns the matrix object supplied as input argument.
-  
-  get <- function() x
+    set <- function(y)
+    {
+        x <<- y
+        inverse <<- NULL
+    }
   
   
-  ## This is third function of the list. By using the "superassignment" operator
-  ## "<<-", an inverse matrix is assigned to an object in the global environment.
+    ## This second function of the list returns the matrix object present in the
+    ## global environment if the set function above is called beforehand. Else it
+    ## returns the matrix object supplied as input argument.
   
-  setInverse <- function(Minverse) inverse <<- Minverse
-  
-  ## This fourth function returns the object present in the global environment if
-  ## the setInverse function above is called beforehand.Else it returns a NULL.
-  
-  getInverse <- function() inverse
+    get <- function() x
   
   
-  ## Return list of functions with names, set, get, setInverse, getInverse.
+    ## This is third function of the list. By using the "superassignment" operator
+    ## "<<-", an inverse matrix is assigned to an object in the global environment.
   
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+    setInverse <- function(Minverse) inverse <<- Minverse
+  
+    ## This fourth function returns the object present in the global environment if
+    ## the setInverse function above is called beforehand.Else it returns a NULL.
+  
+    getInverse <- function() inverse
+  
+  
+    ## Return list of functions with names, set, get, setInverse, getInverse.
+  
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 
 }
 
@@ -58,30 +61,33 @@ makeCacheMatrix <- function(x = matrix()) {
 ## and by retrieving from cache subsequently. The input argument is the list
 ## object returned by the makeCacheMatrix function.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...)
+{  
   
-  inverse <- x$getInverse()
+    inverse <- x$getInverse()
   
-  ## The first time, the above step would have assigned NULL to "inverse" and the 
-  ## block is skipped. Subsequently, however, the inverse matrix located in the
-  ## global environment is returned after printing a message indicating the above.   
+    ## The first time, the above step would have assigned NULL to "inverse" and the 
+    ## block is skipped. Subsequently, however, the inverse matrix located in the
+    ## global environment is returned after printing a message indicating the above.   
   
-  if (!is.null(inverse)){
+    if (!is.null(inverse))
+    {
         message("getting cached matrix")
         return(inverse) 
-  }
+    }
   
-  ## If it is the first time, the matrix is taken and inverse is computed by
-  ## solve() function.   
+    ## If it is the first time, the matrix is taken and inverse is computed by
+    ## solve() function.   
   
-  mat <- x$get()
-  inverse <- solve(mat,...)
+    mat <- x$get()
+    inverse <- solve(mat,...)
   
-  ## And this inverse is stored in the global environment to be returned from    
-  ## there subsequently. 
+    ## And this inverse is stored in the global environment to be returned from    
+    ## there subsequently. 
   
-  x$setInverse(inverse)
+    x$setInverse(inverse)
   
-  ## Return the computed inverse if it is the first time.
-  inverse
+    ## Return the computed inverse if it is the first time.
+    inverse
+    
 }
